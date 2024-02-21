2dac7f2e3795cd53e9baa2ab11b10bd11b02fc6d
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-21 15:02:19 +0530

    If Else Statement

---
+++ CoreJava/src/com/corejava/IfElse.java
@@ -0,0 +1,17 @@
+package com.corejava;
+
+
+/* If-Else Statement Example */
+public class IfElse {
+
+	
+	public static void main(String[] args) {
+		int age= 16;
+		if(age >=18) {
+			System.out.println("Age is 18+ years");
+		}
+		else {
+			System.out.println("Age is less than 18 years");
+		}
+	}
+}
