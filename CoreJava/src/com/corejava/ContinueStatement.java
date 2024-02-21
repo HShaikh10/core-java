068ea47d1a4df9855ea50ad810323d8c0abd1b48
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-21 18:21:34 +0530

    Continue Statement Program

---
+++ CoreJava/src/com/corejava/ContinueStatement.java
@@ -0,0 +1,17 @@
+package com.corejava;
+
+/* Break and Continue Statements*/
+public class ContinueStatement {
+
+	
+	public static void main(String[] args) {
+	
+		for(int i=0;i<=10;i++) {
+			if(i==7) {
+			
+				continue;
+			}	System.out.println(i);
+		}
+	}
+	
+}
