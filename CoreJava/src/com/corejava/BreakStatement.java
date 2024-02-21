fba2b198f895b402686dc4c5941583fbe8688eea
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-21 18:20:43 +0530

    Break Statement Program

---
+++ CoreJava/src/com/corejava/BreakStatement.java
@@ -0,0 +1,17 @@
+package com.corejava;
+
+/* Break and Continue Statements*/
+public class BreakStatement {
+
+	
+	public static void main(String[] args) {
+	
+		for(int i=0;i<=10;i++) {
+			if(i==7) {
+			
+				break;
+			}	System.out.println(i);
+		}
+	}
+	
+}
