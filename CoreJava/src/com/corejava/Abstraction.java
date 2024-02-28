9ea3f56faf9c327bcb1227d0f3ceb57fa93e0168
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 13:23:04 +0530

    Abstraction

---
+++ CoreJava/src/com/corejava/Abstraction.java
@@ -0,0 +1,35 @@
+package com.corejava;
+
+/**
+ * This class is used for abstraction
+ * @author Hasnain 
+ */
+
+abstract class Animal {
+
+	public abstract void animalSound();
+
+	/* Sleep Method */
+
+	public void sleep() {
+		System.out.println("Cat");
+	}
+}
+
+class Cat extends Animal {
+
+	/* Animal Sound method */
+
+	public void animalSound() {
+		System.out.println("Cat");
+	}
+}
+
+class Main {
+	public static void main(String[] args) {
+		Cat cat = new Cat();
+		cat.animalSound();
+		cat.sleep();
+	}
+
+}
