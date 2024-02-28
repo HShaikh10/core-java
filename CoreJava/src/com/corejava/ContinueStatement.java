680d58a0d73c79e17cd0ec9416aba81d82a580ae
HShaikh10 <160571236+HShaikh10@users.noreply.github.com>
2024-02-28 13:01:43 +0530

    Update ContinueStatement.java

--- CoreJava/src/com/corejava/ContinueStatement.java
+++ CoreJava/src/com/corejava/ContinueStatement.java
@@ -1,17 +1,21 @@
 package com.corejava;
 
-/* Break and Continue Statements*/
+/**
+ * This class is used for Continue Statement
+ * @author Hasnain
+ */
+
 public class ContinueStatement {
 
-	
 	public static void main(String[] args) {
-	
-		for(int i=0;i<=10;i++) {
-			if(i==7) {
-			
+
+		for (int i = 0; i <= 10; i++) {
+			if (i == 7) {
+
 				continue;
-			}	System.out.println(i);
+			}
+			System.out.println(i);
 		}
 	}
-	
+
 }
