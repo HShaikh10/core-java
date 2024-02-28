4761330aca9f641ec086de01533d79f34bb2417f
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 15:58:43 +0530

    Hello World Example

--- CoreJava/src/com/corejava/HelloWorld.java
+++ CoreJava/src/com/corejava/HelloWorld.java
@@ -4,14 +4,11 @@ package com.corejava;
  * This class is used for Hello World Example
  * @author Hasnain
  */
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
-
-<<<<<<< Updated upstream
+	
 }
-=======
-}
->>>>>>> Stashed changes
