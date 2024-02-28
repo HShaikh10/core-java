51175c43d4fb500dbf86ef3e2ad5aa6b8f08bde7
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 13:26:31 +0530

    Exception Handling using Throws

---
+++ CoreJava/src/com/corejava/ExceptionHandlerThrows.java
@@ -0,0 +1,26 @@
+package com.corejava;
+/**
+ * This class is used for Exception Handler using Throws Example
+ * @author Hasnain
+ */
+
+public class ExceptionHandlerThrows {
+
+	class Test {
+		
+		/* static method for arithmetic exception using throwss keyword */
+		static void check() throws ArithmeticException {
+			System.out.println("Inside check function");
+			throw new ArithmeticException("demo");
+		}
+
+		public static void main(String args[]) {
+			try {
+				check();
+			} catch (ArithmeticException e) {
+				System.out.println("caught" + e);
+			}
+		}
+	}
+
+}
