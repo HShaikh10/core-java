a388aedae954fbd3dd108c489e49381b63e7d629
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 13:21:45 +0530

    This Keyword

---
+++ CoreJava/src/com/corejava/ThisKeyword.java
@@ -0,0 +1,21 @@
+package com.corejava;
+
+/**
+ * This class is used for This Keyword Example
+ * @author Hasnain
+ */
+
+public class ThisKeyword {
+
+	int x;
+
+	public ThisKeyword(int x) {
+		this.x = x;
+	}
+
+	public static void main(String[] args) {
+		ThisKeyword th = new ThisKeyword(5);
+		System.out.println("Value of x is: " + th.x);
+	}
+
+}
