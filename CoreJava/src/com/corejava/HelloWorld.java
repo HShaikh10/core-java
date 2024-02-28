b8fde4c52cc5aa2426bd3f093da64eaa50eb5bb3
HShaikh10 <160571236+HShaikh10@users.noreply.github.com>
2024-02-28 12:59:00 +0530

    Update HelloWorld.java

--- CoreJava/src/com/corejava/HelloWorld.java
+++ CoreJava/src/com/corejava/HelloWorld.java
@@ -1,9 +1,13 @@
 package com.corejava;
 
-/* Hello World */
+/**
+ * This class is used for Hello World Example
+ * @author Hasnain
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
+
 }
