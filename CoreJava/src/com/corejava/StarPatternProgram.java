2b259405f9eadf34c0e2874e1dd59dc8ac0af87c
HShaikh10 <160571236+HShaikh10@users.noreply.github.com>
2024-02-28 13:00:59 +0530

    Update StarPatternProgram.java

--- CoreJava/src/com/corejava/StarPatternProgram.java
+++ CoreJava/src/com/corejava/StarPatternProgram.java
@@ -1,21 +1,24 @@
 package com.corejava;
 
-/* Star Pattern Program */
+/**
+ * This class is used for star pattern Example
+ * @author Hasnain
+ */
 
 public class StarPatternProgram {
 
 	public static void main(String[] args) {
-		
+
 		int i;
 		int j;
 		int row = 5;
-		
-		for(i = 0; i <= row; i++) {
-			for(j = 0;j < i; j++) {
+
+		for (i = 0; i <= row; i++) {
+			for (j = 0; j < i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 	}
-	
+
 }
