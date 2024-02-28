e98998eef2c9ac16e3d7c3e705409c6bfe04b415
HShaikh10 <160571236+HShaikh10@users.noreply.github.com>
2024-02-28 13:01:18 +0530

    Update BreakStatement.java

--- CoreJava/src/com/corejava/BreakStatement.java
+++ CoreJava/src/com/corejava/BreakStatement.java
@@ -1,17 +1,19 @@
 package com.corejava;
 
-/* Break and Continue Statements*/
+/**
+ * This class is used for Break Statement
+ * @author Hasnain
+ */
 public class BreakStatement {
 
-	
 	public static void main(String[] args) {
-	
-		for(int i=0;i<=10;i++) {
-			if(i==7) {
-			
+
+		for (int i = 0; i <= 10; i++) {
+			if (i == 7) {
 				break;
-			}	System.out.println(i);
+			}
+			System.out.println(i);
 		}
 	}
-	
+
 }
