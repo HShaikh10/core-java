5bae0c8a215ce475e095352a5aeffe7b29660488
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 13:26:17 +0530

    Exception Handling using Throw

---
+++ CoreJava/src/com/corejava/ExceptionHandlerThrow.java
@@ -0,0 +1,27 @@
+package com.corejava;
+/**
+ * This class is used for Exception Handler using Throw Example
+ * @author Hasnain
+ */
+
+public class ExceptionHandlerThrow {
+
+	class Test {
+		
+		/*
+		 * Static method for Arithmetic Exception 
+		 */
+		static void avg() {
+			try {
+				throw new ArithmeticException("demo");
+			} catch (ArithmeticException e) {
+				System.out.println("Exception caught");
+			}
+		}
+
+		public static void main(String args[]) {
+			avg();
+		}
+	}
+
+}
