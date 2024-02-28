6dbe02bf824cf0aa8d9500fc6f379d8993ff01c3
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 13:24:35 +0530

    Hierarchy Inheritance

---
+++ CoreJava/src/com/corejava/HierarchyInheritance.java
@@ -0,0 +1,42 @@
+package com.corejava;
+
+/**
+ * This class is used for Hierarchy Inheritance Example
+ * @author Hasnain
+ */
+
+class Animal {
+	
+	/* Method for eat*/
+	void eat() {
+		System.out.println("eating...");
+	}
+	
+}
+
+class Dog extends Animal {
+	
+	/* Method for bark*/
+	void bark() {
+		System.out.println("barking...");
+	}
+	
+}
+
+class Cat extends Animal {
+	
+	/* Method for meow*/
+	void meow() {
+		System.out.println("meowing...");
+	}
+	
+}
+
+class TestInheritance3 {
+	public static void main(String args[]) {
+		Cat c = new Cat();
+		c.meow();
+		c.eat();
+	}
+	
+}
