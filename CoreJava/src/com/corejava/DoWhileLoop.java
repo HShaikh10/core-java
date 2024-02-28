342a5614107093fbde86d1b737bd00eb0cf9fb68
HShaikh10 <160571236+HShaikh10@users.noreply.github.com>
2024-02-28 13:00:38 +0530

    Update DoWhileLoop.java

--- CoreJava/src/com/corejava/DoWhileLoop.java
+++ CoreJava/src/com/corejava/DoWhileLoop.java
@@ -1,15 +1,19 @@
 package com.corejava;
 
+/**
+ * This class is used for Do While Loop Example
+ * @author Hasnain
+ */
+
 public class DoWhileLoop {
-	
+
 	public static void main(String[] args) {
-	
-		int i=0;
+
+		int i = 0;
 		do {
 			System.out.println(i);
 			i++;
-		}
-		while(i <= 10);
+		} while (i <= 10);
 	}
-	
+
 }
