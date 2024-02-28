b22686dc776b1727aaa0b669281d4aff762ace17
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 16:01:26 +0530

    Set Collection

--- CoreJava/src/com/corejava/SetCollection.java
+++ CoreJava/src/com/corejava/SetCollection.java
@@ -5,20 +5,20 @@ import java.util.HashSet;
 /**
  * This class is used for Set Collection Example
  * @author Hasnain
- */
+*/
 
 public class SetCollection {
 
 	public static void main(String[] args) {
 
-		HashSet<String> set = new HashSet<String>();
-		set.add("BMW");
-		set.add("Volvo");
-		set.add("Mazda");
-		set.add("Volvo");
-		set.add("Ford");
+		HashSet<String> hashSet = new HashSet<String>();
+		hashSet.add("BMW");
+		hashSet.add("Volvo");
+		hashSet.add("Mazda");
+		hashSet.add("Volvo");
+		hashSet.add("Ford");
 
-		System.out.println(set);
+		System.out.println(hashSet);
 
 	}
 
