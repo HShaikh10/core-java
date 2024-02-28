bfd79d43e1c68db92d419c61a664f8ad7a18e59a
HShaikh10 <160571236+HShaikh10@users.noreply.github.com>
2024-02-28 12:59:27 +0530

    Update IfElse.java

--- CoreJava/src/com/corejava/IfElse.java
+++ CoreJava/src/com/corejava/IfElse.java
@@ -1,14 +1,16 @@
 package com.corejava;
 
-/* If-Else Statement Example */
+/**
+ * This class is used for IF-Else Example
+ * @author Hasnain
+ */
 public class IfElse {
-	
+
 	public static void main(String[] args) {
-		int age= 16;
-		if (age >=18) {
+		int age = 16;
+		if (age >= 18) {
 			System.out.println("Age is 18+ years");
-		}
-		else {
+		} else {
 			System.out.println("Age is less than 18 years ");
 		}
 	}
