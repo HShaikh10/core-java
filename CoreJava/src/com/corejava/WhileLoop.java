0864f4726f775c50adf591073ac24c72676494c6
HShaikh10 <160571236+HShaikh10@users.noreply.github.com>
2024-02-28 13:00:19 +0530

    Update WhileLoop.java

--- CoreJava/src/com/corejava/WhileLoop.java
+++ CoreJava/src/com/corejava/WhileLoop.java
@@ -1,15 +1,18 @@
 package com.corejava;
 
-/* While Loop */
+/**
+ * This class is used for While Loop Example
+ * @author Hasnain
+ */
 public class WhileLoop {
 
 	public static void main(String[] args) {
 		int i = 0;
-		
-	    while (i <= 10) {
-	      System.out.println(i);
-	      i++;
-	    }  
+
+		while (i <= 10) {
+			System.out.println(i);
+			i++;
+		}
 	}
-	
+
 }
