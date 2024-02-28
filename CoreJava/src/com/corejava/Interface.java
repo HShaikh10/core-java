a017be7f9839730ebcd993fa03af97fd832bb4bf
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 13:25:41 +0530

    Interface

---
+++ CoreJava/src/com/corejava/Interface.java
@@ -0,0 +1,32 @@
+package com.corejava;
+
+/**
+ * This class is used for Interface Example
+ * @author Hasnain
+ */
+
+interface Drawable {
+	/* static method for draw */
+	void draw();
+}
+
+class Rectangle implements Drawable {
+	/* method for draw rectangle */
+	public void draw() {
+		System.out.println("drawing rectangle");
+	}
+}
+
+class Circle implements Drawable {
+	/* method for draw circle */
+	public void draw() {
+		System.out.println("drawing circle");
+	}
+}
+
+class TestInterface1 {
+	public static void main(String args[]) {
+		Drawable d = new Circle();
+		d.draw();
+	}
+}
