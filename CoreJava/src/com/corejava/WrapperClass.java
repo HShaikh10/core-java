d04a73de4a54d2ef82181e5341b582c980b305bc
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 13:19:10 +0530

    Wrapper Classes

---
+++ CoreJava/src/com/corejava/WrapperClass.java
@@ -0,0 +1,17 @@
+package com.corejava;
+
+/**
+ * This class is used for Wrapper Class Example
+ * @author Hasnain
+ */
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+
+		Integer i = new Integer(10);
+		int j = 10;
+		System.out.println(i == j);
+	}
+
+}
