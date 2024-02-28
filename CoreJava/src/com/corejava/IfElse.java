cd01bf88a42b9dd1977ac2921a5ea6bf478ed007
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 15:59:13 +0530

    If-Else Staement

--- CoreJava/src/com/corejava/IfElse.java
+++ CoreJava/src/com/corejava/IfElse.java
@@ -1,13 +1,18 @@
 package com.corejava;
 
+import java.util.Scanner;
+
 /**
  * This class is used for IF-Else Example
  * @author Hasnain
- */
+*/
+
 public class IfElse {
 
 	public static void main(String[] args) {
-		int age = 16;
+		Scanner scanner = new Scanner(System.in);
+		int age = scanner.nextInt();
+		
 		if (age >= 18) {
 			System.out.println("Age is 18+ years");
 		} else {
