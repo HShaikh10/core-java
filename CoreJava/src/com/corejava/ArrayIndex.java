96fedaf41161653c7b2a20403597ad8ae0f5e6f3
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 13:27:09 +0530

    Array

---
+++ CoreJava/src/com/corejava/ArrayIndex.java
@@ -0,0 +1,28 @@
+package com.corejava;
+
+/**
+ * This class is used for Array Index Number
+ * @author Hasnain
+ */
+public class ArrayIndex {
+
+	/* Static method for index number with parameters */
+
+	public static int IndexNumber(int array[], int element) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == element) {
+				return i;
+			}
+		}
+		return -1;
+
+	}
+
+	public static void main(String[] args) {
+		int array[] = { 1, 2, 3, 4, 5, 6 };
+		int element = 2;
+		int index = IndexNumber(array, element);
+		System.out.println(array);
+	}
+
+}
