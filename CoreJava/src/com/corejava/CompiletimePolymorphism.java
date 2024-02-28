611ea46919bdd81e21427b30d667255ce0c657a6
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 15:56:32 +0530

    Compile Time Polymorphism

--- CoreJava/src/com/corejava/CompiletimePolymorphism.java
+++ CoreJava/src/com/corejava/CompiletimePolymorphism.java
@@ -4,12 +4,13 @@ package com.corejava;
  * This class is used for Compile time Polymorphism Example
  * @author Hasnain
  */
+
 public class CompiletimePolymorphism {
 
 	public static void main(String args[]) {
-		CompileTimePloymorphismExample obj = new CompileTimePloymorphismExample();
-		obj.display();
-		obj.display("Polymorphism");
+		CompileTimePloymorphismExample comileTimePolymorphismExample = new CompileTimePloymorphismExample();
+		comileTimePolymorphismExample.display();
+		comileTimePolymorphismExample.display("Polymorphism");
 	}
 
 }
@@ -18,14 +19,16 @@ class CompileTimePloymorphismExample {
 
 	/*
 	 * The display method is used for without parameter example
-	 */
+	*/
+	
 	void display() {
 		System.out.println("In Display without parameter");
 	}
 
 	/*
 	 * The display method is used for with parameter example
-	 */
+	*/
+	
 	void display(String value) {
 		System.out.println("In Display with parameter" + value);
 	}
