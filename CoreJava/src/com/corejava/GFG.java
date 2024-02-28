a3eacab209ddfd2060add3618526579ad1069204
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 15:16:13 +0530

    myTestFile Read class GFG

--- CoreJava/src/com/corejava/GFG.java
+++ CoreJava/src/com/corejava/GFG.java
@@ -1,42 +1,32 @@
 package com.corejava;
+
 import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.*;
 
+public class GFG {
+	public static List<String> readFileInList(String fileName) {
 
-	// Java program to illustrate reading data from file
-	// using nio.File
-	import java.io.*;
-	import java.nio.charset.StandardCharsets;
-	import java.nio.file.*;
-	import java.util.*;
-	  public class GFG {
-		public static List<String>
-		readFileInList(String fileName)
-		{
-
-			List<String> lines = Collections.emptyList();
-			try {
-				lines = Files.readAllLines(
-					Paths.get(fileName),
-					StandardCharsets.UTF_8);
-			}
-
-			catch (IOException e) {
-
-				// do something
-				e.printStackTrace();
-			}
-			return lines;
+		List<String> lines = Collections.emptyList();
+		try {
+			lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
 		}
-		public static void main(String[] args)
-		{
-			List l = readFileInList(
-				"D:\\Sample\\core-java\\CoreJava\\TestFile.txt");
 
-			Iterator<String> itr = l.iterator();
-			while (itr.hasNext())
-				System.out.println(itr.next());
+		catch (IOException e) {
+
+			e.printStackTrace();
 		}
+		return lines;
 	}
 
+	public static void main(String[] args) {
+		List list = readFileInList("D:\\Sample\\core-java\\CoreJava\\TestFile.txt");
 
-
+		Iterator<String> itr = list.iterator();
+		while (itr.hasNext())
+			System.out.println(itr.next());
+	}
+	
+}
