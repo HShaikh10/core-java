d1d903c5428e9b84fad817a02737cb9945fcd569
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-02-28 13:22:39 +0530

    Static Methods

---
+++ CoreJava/src/com/corejava/StaticMethods.java
@@ -0,0 +1,31 @@
+package com.corejava;
+
+/**
+ * This class is used for Static Methods Example
+ * @author Hasnain
+ */
+
+public class StaticMethods {
+
+	static int x = 10;
+	static int y;
+
+	/* Static method for function */
+	static void func(int z) {
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		System.out.println("z = " + z);
+	}
+
+	/* Static method for print */
+
+	static {
+		System.out.println("Running static initialization block.");
+		y = x + 5;
+	}
+
+	public static void main(String args[]) {
+		func(8);
+	}
+
+}
