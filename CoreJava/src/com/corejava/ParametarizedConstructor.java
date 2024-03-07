d778d9ed54a55e17d99c0d61f34b0e006f5bba08
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 18:57:58 +0530

    Parametarized Constructor Example

--- CoreJava/src/com/corejava/ParametarizedConstructor.java
+++ CoreJava/src/com/corejava/ParametarizedConstructor.java
@@ -7,15 +7,12 @@ package com.corejava;
 public class ParametarizedConstructor {
 
 	String cities;
-
 	/* Parameterized Constructor */
 	public ParametarizedConstructor(String c) {
 		cities = c;
 		System.out.println("Cities: " + c);
 	}
-
 	public static void main(String[] args) {
-
 		ParametarizedConstructor parametarizedConstructor = new ParametarizedConstructor("Ahmedabad");
 		ParametarizedConstructor parametarizedConstructor2 = new ParametarizedConstructor("Baroda");
 	}
