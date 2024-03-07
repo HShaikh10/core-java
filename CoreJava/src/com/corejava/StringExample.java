91bebecf86b8beb25e13b6fcd738cfc456fa85cd
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 18:59:03 +0530

    String Example

--- CoreJava/src/com/corejava/StringExample.java
+++ CoreJava/src/com/corejava/StringExample.java
@@ -1,5 +1,7 @@
 package com.corejava;
 
+import java.util.Scanner;
+
 /**
  * This class is used for String Example Example
  * @author Hasnain
@@ -7,18 +9,31 @@ package com.corejava;
 
 public class StringExample {
 
-	public static void main(String[] args) {
-		String str = "User";
-		String str2 = "Test";
-
-		System.out.println(str.concat(str2));
-		System.out.println(str.equals(str2));
-		System.out.println(str.charAt(2));
-		System.out.println(str.toLowerCase());
-		System.out.println(str2.toUpperCase());
-		System.out.println(str.compareTo(str2));
-		System.out.println(str2.length());
-		System.out.println(str.split(str2));
-	}
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first string:");
+        String string = scanner.nextLine();
+        System.out.println("Enter the second string:");
+        String string2 = scanner.nextLine();
+        System.out.println("Concatenation: " + string.concat(string2));
+        System.out.println("Equals: " + string.equals(string2)); 
+        
+        if (string.length() >= 3) {
+            System.out.println("Character at index 2: " + string.charAt(2));
+        } else {
+            System.out.println("First string is too short to get character at index 2.");
+        }
+        
+        System.out.println("Lowercase: " + string.toLowerCase());
+        System.out.println("Uppercase: " + string2.toUpperCase());
+        System.out.println("Comparison: " + string.compareTo(string2));
+        System.out.println("Length of second string: " + string2.length());
+        System.out.println("Splitting first string using second string: ");
+        
+        for (String s : string.split(string2)) {
+            System.out.println(s);
+        }
+        scanner.close();
+    }
+    
 }
