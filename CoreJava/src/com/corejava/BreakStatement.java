0c5501b8a1170b9a7d78ae5d17b0659f2f72173d
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 18:53:13 +0530

    Break Statement Example

--- CoreJava/src/com/corejava/BreakStatement.java
+++ CoreJava/src/com/corejava/BreakStatement.java
@@ -12,11 +12,19 @@ public class BreakStatement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int value;
+        boolean conditionMet = false;
         for (int i = 0; i <= 10; i++) {
-        	System.out.println("Enter Value for Break Statement");
-            value = scanner.nextInt();
-            
-            break;
+            System.out.println("Enter Value for Break Statement");
+            value = scanner.nextInt();            
+            if (value == 0) {
+                conditionMet = true;
+                break;
+            }
+        }
+        if (conditionMet) {
+            System.out.println("Loop exited due to condition being met.");
+        } else {
+            System.out.println("Loop completed without meeting the condition.");
         }
     }
     
