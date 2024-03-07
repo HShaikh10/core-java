951d3bf3b4294c12d8265c9bf74082249e2a3c85
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 15:55:45 +0530

    Interface Example

--- CoreJava/src/com/corejava/Interface.java
+++ CoreJava/src/com/corejava/Interface.java
@@ -8,31 +8,32 @@ package com.corejava;
 interface Drawable {
 	
 	/* static method for draw */
-	
 	void draw();
+	
 }
 
-class Rectangle implements Drawable {
+public class Rectangle implements Drawable {
 	
 	/* method for draw rectangle */
-	
 	public void draw() {
 		System.out.println("drawing rectangle");
 	}
+	
 }
 
-class Circle implements Drawable {
+public class Circle implements Drawable {
 	
 	/* method for draw circle */
-	
 	public void draw() {
 		System.out.println("drawing circle");
 	}
+	
 }
 
-class TestInterface1 {
+public class TestInterface1 {
 	public static void main(String args[]) {
-		Drawable d = new Circle();
-		d.draw();
+		Drawable drawable = new Circle();
+		drawable.draw();
 	}
+	
 }
