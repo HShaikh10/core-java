ef113abc5599b61afd7163963e099000cb86a997
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 18:55:08 +0530

    Exception Handler Final Example

--- CoreJava/src/com/corejava/ExceptionHandlerFinal.java
+++ CoreJava/src/com/corejava/ExceptionHandlerFinal.java
@@ -1,5 +1,7 @@
 package com.corejava;
 
+import java.util.Scanner;
+
 /**
  * This class is used for Exception Handler using Final Example
  * @author Hasnain
@@ -7,16 +9,18 @@ package com.corejava;
 
 public class ExceptionHandlerFinal {
 
-	public static void main(String[] args) {
-		int a[] = new int[2];
-		System.out.println("out of try");
-		
-		try {
-			System.out.println("Access invalid element" + a[3]);
-		} 
-		finally {
-			System.out.println("finally is always executed.");
-		}
-	}
+    public static void main(String[] args) {
+        int a[] = new int[2];
+        System.out.println("out of try");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Access invalid element: " + a[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index out of bounds.");
+        } finally {
+            System.out.println("Finally block is always executed.");
+        }       
+        scanner.close();
+    }
 
 }
