8d48178dc54aea3e7a51d9f34cf98178ab597443
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 15:56:04 +0530

    Map Collection Example

--- CoreJava/src/com/corejava/MapCollection.java
+++ CoreJava/src/com/corejava/MapCollection.java
@@ -10,17 +10,17 @@ import java.util.HashMap;
 public class MapCollection {
 
 	public static void main(String[] args) {
-		HashMap<Integer, String> capitalCities = new HashMap<Integer, String>();
+		HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
 
-		capitalCities.put(1, "London-England");
-		capitalCities.put(2, "Washington-USA");
-		capitalCities.put(3, "Delhi-India");
-		capitalCities.put(4, "Berlin-Germany");
+		hashMap.put(1, "London-England");
+		hashMap.put(2, "Washington-USA");
+		hashMap.put(3, "Delhi-India");
+		hashMap.put(4, "Berlin-Germany");
 
-		for (String i : capitalCities.values()) {
+		for (String i : hashMap.values()) {
 			System.out.println(i);
 		}
-		System.out.println(capitalCities);
+		System.out.println(hashMap);
 
 	}
 
