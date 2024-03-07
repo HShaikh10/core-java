d2a13941d61bd222a224d890a24d3f0a520b1dce
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 18:59:15 +0530

    String Methods Example

--- CoreJava/src/com/corejava/StringMethod2.java
+++ CoreJava/src/com/corejava/StringMethod2.java
@@ -1,32 +1,38 @@
 package com.corejava;
 
-/**
- * This class is used for String, Integer, Bigdecimal Example
- * @author Hasnain
-*/
-
 import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * This class is used for String, Integer, BigDecimal Example
+ * @author Hasnain
+ */
 
 public class StringMethod2 {
 
-	public static void main(String[] args) {
-		String string1 = "Radar";
-		String string2 = "Column";
-
-		System.out.println(string1 == string2);
-		System.out.println(string1.equals(string2));
-
-		Integer integer1 = 16;
-		Integer integer2 = 16;
-		System.out.println(integer1 == integer2);
-		System.out.println(integer1.equals(integer2));
-
-		BigDecimal bigDecimal1 = new BigDecimal("10.10");
-		BigDecimal bigDecimal2 = new BigDecimal("20.00");
-
-		System.out.println(bigDecimal1 == bigDecimal2);
-		System.out.println(bigDecimal1.equals(bigDecimal2));
-
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);        
+        System.out.println("Enter the first string:");
+        String string1 = scanner.nextLine();
+        System.out.println("Enter the second string:");
+        String string2 = scanner.nextLine();
+        System.out.println("String comparison using ==: " + (string1 == string2));
+        System.out.println("String comparison using equals method: " + string1.equals(string2));
+        
+        System.out.println("Enter the first integer:");
+        int integer1 = scanner.nextInt();
+        System.out.println("Enter the second integer:");
+        int integer2 = scanner.nextInt();
+        System.out.println("Integer comparison using ==: " + (integer1 == integer2));
+        System.out.println("Integer comparison using equals method: " + Integer.valueOf(integer1).equals(integer2));
+        
+        System.out.println("Enter the first BigDecimal:");
+        BigDecimal bigDecimal1 = new BigDecimal(scanner.next());
+        System.out.println("Enter the second BigDecimal:");
+        BigDecimal bigDecimal2 = new BigDecimal(scanner.next());
+        System.out.println("BigDecimal comparison using ==: " + (bigDecimal1 == bigDecimal2));
+        System.out.println("BigDecimal comparison using equals method: " + bigDecimal1.equals(bigDecimal2));
+        scanner.close();
+    }
 
 }
