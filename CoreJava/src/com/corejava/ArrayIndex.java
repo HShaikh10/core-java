23ed93611162cc71658f458b0abe145c2a706209
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 18:53:00 +0530

    ArrayIndex Example

--- CoreJava/src/com/corejava/ArrayIndex.java
+++ CoreJava/src/com/corejava/ArrayIndex.java
@@ -9,27 +9,27 @@ import java.util.Scanner;
 
 public class ArrayIndex {
 
-	/* Static method for index number with parameters */
-	public static int IndexNumber(int array[], int element) {
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] == element) {
-				return i;
-			}
-		}
-		return -1;
-		
-}
+    /* Static method for index number with parameters */
+    public static int indexNumber(int array[], int element) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int IndexNumber = scanner.nextInt();
-		
-		System.out.println("Enter number for IndexNumber: ");
-		int array[] = { 1, 2, 3, 4, 5, 6 };
-		int element = 2;
-		
-		int index = IndexNumber(array, element);
-		System.out.println(array);
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number for IndexNumber: ");
+        int element = scanner.nextInt();
+        int array[] = { 1, 2, 3, 4, 5, 6 };
+        int index = indexNumber(array, element);
+        if (index != -1) {
+            System.out.println("Index of " + element + " in the array is: " + index);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
 
 }
