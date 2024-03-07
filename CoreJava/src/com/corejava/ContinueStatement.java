097ca342b759c5664a36410fae1855df4d445097
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 18:54:23 +0530

    Continue Statement Example

--- CoreJava/src/com/corejava/ContinueStatement.java
+++ CoreJava/src/com/corejava/ContinueStatement.java
@@ -11,18 +11,15 @@ public class ContinueStatement {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         for (int i = 0; i <= 10; i++) {
             System.out.println("Enter 0 to continue or any other number to print the current number: ");
             int userInput = scanner.nextInt();
             if (userInput == 0) {
                 continue;
             }
-            
             System.out.println(i);
         }
-        
-            scanner.close();
+        scanner.close();
     }
-    
+
 }
