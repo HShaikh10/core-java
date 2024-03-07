a88aede9296a8643fbb0d1a38384789a8fb44acc
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 15:58:53 +0530

    Super Keyword Example

--- CoreJava/src/com/corejava/SuperKeyword.java
+++ CoreJava/src/com/corejava/SuperKeyword.java
@@ -4,10 +4,9 @@ package com.corejava;
  * This class is used for Super Keyword Example
  * @author Hasnain
  */
-class Bird {
+public class Bird {
 
 	/* method for bird sound */
-	
 	public void birdSound() {
 
 		System.out.println("Bird make sound");
@@ -15,19 +14,19 @@ class Bird {
 
 }
 
-class Pigeon extends Bird {
+public class Pigeon extends Bird {
 
-	/* method for bird sound using Super keyword */
-
-	public void birdSound() {
+   /* method for bird sound using Super keyword */
+   public void birdSound() {
 		super.birdSound();
 		System.out.println("Pigeon sound");
 	}
 
 }
 
-public class SuperKeyword {
-	public static void main(String[] args) {
+   public class SuperKeyword {
+
+	   public static void main(String[] args) {
 		Bird bird = new Bird();
 		bird.birdSound();
 	}
