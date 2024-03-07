c2e079e4c86e8ca637c4bbc8359fc2524aca8678
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 18:56:01 +0530

    File Example

--- CoreJava/src/com/corejava/GFG.java
+++ CoreJava/src/com/corejava/GFG.java
@@ -12,26 +12,22 @@ import java.util.*;
 */
 
 public class GFG {
+	
 	public static List<String> readFileInList(String fileName) {
-
 		List<String> lines = Collections.emptyList();
 		try {
 			lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
 		}
-
 		catch (IOException e) {
-
 			e.printStackTrace();
 		}
 		return lines;
 	}
-
 	public static void main(String[] args) {
 		List list = readFileInList("D:\\Sample\\core-java\\CoreJava\\TestFile.txt");
-
-		Iterator<String> itr = list.iterator();
-		while (itr.hasNext())
-			System.out.println(itr.next());
+		Iterator<String> stringIterator = list.iterator();
+		while (stringIterator.hasNext())
+		System.out.println(stringIterator.next());
 	}
 	
 }
