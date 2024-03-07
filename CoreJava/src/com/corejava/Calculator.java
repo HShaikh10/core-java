7902d4d274dfa9a7aa4a4bbc275a34b6e227d447
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 18:53:25 +0530

    Calculator Example

--- CoreJava/src/com/corejava/Calculator.java
+++ CoreJava/src/com/corejava/Calculator.java
@@ -9,27 +9,28 @@ import java.util.Scanner;
 
 public class Calculator {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter Principle Amount");
+            double principle = scanner.nextDouble();
 
-		double principle;
-		double rate;
-		double time;
-		double emi;
+            System.out.println("Enter Rate: ");
+            double rate = scanner.nextDouble();
 
-		System.out.println("Enter Principle Amount");
-		principle = scanner.nextFloat();
+            System.out.println("Enter time: ");
+            double time = scanner.nextDouble();
 
-		System.out.println("Enter Rate: ");
-		rate = scanner.nextFloat();
+            rate = rate / (12 * 100);
+            time = time * 12;
 
-		System.out.println("Enter time: ");
-		time = scanner.nextFloat();
-
-		rate = rate / (12 * 100);
-		time = time * 12;
-		emi = (principle * rate * Math.pow(1 + rate, time)) / (Math.pow(1 + rate, time) - 1);
-		System.out.println("Monthly Emi is: " + emi + "\n");
-	}
-
+            double emi = (principle * rate * Math.pow(1 + rate, time)) / (Math.pow(1 + rate, time) - 1);
+            System.out.println("Monthly Emi is: " + emi + "\n");
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter valid numeric values.");
+        } finally {
+            scanner.close();
+        }
+    }
+    
 }
