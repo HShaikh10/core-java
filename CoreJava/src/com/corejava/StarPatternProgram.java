adc5d606b88475e4a07498db9e1c78de4727de9c
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 18:58:36 +0530

    Star Pattern Example

--- CoreJava/src/com/corejava/StarPatternProgram.java
+++ CoreJava/src/com/corejava/StarPatternProgram.java
@@ -8,11 +8,9 @@ package com.corejava;
 public class StarPatternProgram {
 
 	public static void main(String[] args) {
-
 		int i;
 		int j;
 		int row = 5;
-
 		for (i = 0; i <= row; i++) {
 			for (j = 0; j < i; j++) {
 				System.out.print("*");
