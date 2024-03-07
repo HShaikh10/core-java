4e87da440dca86cd1b7cfbe8ea7d68d63ff16bc2
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 18:54:09 +0530

    Constructor Example

--- CoreJava/src/com/corejava/ConstructorExample.java
+++ CoreJava/src/com/corejava/ConstructorExample.java
@@ -9,32 +9,25 @@ import java.util.Scanner;
 
 public class ConstructorExample {
 
-	int a;
-	int b;
-	int total;
-
-	public ConstructorExample() {
-
-		Scanner scanner = new Scanner(System.in);
-		
+		public ConstructorExample() {
+		Scanner scanner = new Scanner(System.in);	
+		int a;
 		System.out.println("Enter First Number: ");
 		a = scanner.nextInt();
-
+		int b;
 		System.out.println("Enter Second Number: ");
 		b = scanner.nextInt();
 		scanner.close();
 	}
-
+	int total;
 	/* The add method do addition on variable a and b */
 	public void add() {
 		total = a + b;
 	}
-
 	/*  The add method display addition of variable a and b */
 	public void display() {
 		System.out.println("Total of a and b is: " + total);
 	}
-
 	public static void main(String[] args) {
 		ConstructorExample constructorExample = new ConstructorExample();
 		constructorExample.add();
