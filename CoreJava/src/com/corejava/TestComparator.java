7a4e812f111109daabaeba3d66b515b4844453f6
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-07 19:00:11 +0530

    Comparator Example

--- CoreJava/src/com/corejava/TestComparator.java
+++ CoreJava/src/com/corejava/TestComparator.java
@@ -9,17 +9,15 @@ import java.util.Collections;
 */
 
 public class TestComparator {
+	
 	public static void main(String[] args) {
-
 		ArrayList<String> arrayList = new ArrayList<String>();
 		arrayList.add("Mumbai");
 		arrayList.add("Ahmedabad");
 		arrayList.add("Delhi");
 		arrayList.add("Udaypur");
-
 		Collections.sort(arrayList);
 		System.out.println("List is sorted in reversed order");
-
 		System.out.println(arrayList);
 
 	}
