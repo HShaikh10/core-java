9954dfc49b988d982643498c8bea33db4bbdb0ad
Bhautik Padmani <bhautik.padmani@qdevtechnolab.com>
2024-03-08 13:06:27 +0530

    ListIterator Example

---
+++ CoreJava/src/com/corejava/ListIteratorExample.java
@@ -0,0 +1,33 @@
+package com.corejava;
+
+import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.*;
+
+/**
+ * This class is used for myTestFile Read File Example
+ * @author Hasnain
+*/
+
+public class ListIteratorExample {
+	
+	public static List<String> readFileInList(String fileName) {
+		List<String> lines = Collections.emptyList();
+		try {
+			lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return lines;
+}
+	public static void main(String[] args) {
+		List list = readFileInList("D:\\Sample\\core-java\\CoreJava\\TestFile.txt");
+		Iterator<String> stringIterator = list.iterator();
+		while (stringIterator.hasNext())
+		System.out.println(stringIterator.next());
+	}
+	
+}
